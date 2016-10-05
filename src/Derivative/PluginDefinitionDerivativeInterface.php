840b651da8a23a5ccbc7e1c2e18161b7f0f5ba0e
Kris <kris@meridian-ds.com>
2016-10-05 12:03:01 -0400

    Updating PluginDefinitionDerivativeInterface to remove issues found after updating our codeclimate settings.

--- src/Derivative/PluginDefinitionDerivativeInterface.php
+++ src/Derivative/PluginDefinitionDerivativeInterface.php
@@ -1,21 +1,26 @@
 <?php
 
-/**
- * @file
- * Contains \EclipseGc\Plugin\Derivative\PluginDefinitionDerivativeInterface.
- */
-
 namespace EclipseGc\Plugin\Derivative;
 
 use EclipseGc\Plugin\PluginDefinitionInterface;
 
-interface PluginDefinitionDerivativeInterface extends PluginDefinitionInterface{
+/**
+ * An interface to implement for plugin definitions providing derivatives.
+ *
+ * Plugin definitions can be iterated over via a deriver to create multiple
+ * instances of the same plugin with different plugin definitions. A deriver
+ * class provides this capability and must be documented in the plugin
+ * definition.
+ *
+ */
+interface PluginDefinitionDerivativeInterface extends PluginDefinitionInterface {
 
   /**
-   * Get the deriver class for this plugin definition.
+   * Get the deriver class or string representation for this plugin definition.
    *
    * @return string
+   *   A class or string to be resolved as a plugin deriver.
    */
-  public function getDeriver() : string ;
+  public function getDeriver() : string;
 
 }
