f928160378601d735868a115a2b1090077062cb7
Kris <kris@meridian-ds.com>
2016-10-21 10:11:38 -0500

    improving the docs to meet the required standard.

--- src/PluginDefinitionInterface.php
+++ src/PluginDefinitionInterface.php
@@ -1,27 +1,29 @@
 <?php
 
-/**
- * @file
- * Contains \EclipseGc\Plugin\PluginDefinitionInterface.
- */
-
 namespace EclipseGc\Plugin;
 
+/**
+ * The base interface for defining plugin definitions.
+ */
 interface PluginDefinitionInterface {
 
   /**
    * Gets an array of properties on the plugin definition by key value pair.
    *
    * @return array
+   *   An array of available properties on the definition.
    */
-  public function getProperties() : array ;
+  public function getProperties() : array;
 
   /**
    * Gets the value of a key in the plugin definition.
    *
    * @param string $name
+   *   The name of the property value to retrieve.
    *
    * @return mixed
+   *   The value of the requested property.
+   *
    * @throws \EclipseGc\Plugin\Exception\NonExistentPluginDefinitionPropertyException
    */
   public function getProperty($name);
@@ -30,8 +32,9 @@ interface PluginDefinitionInterface {
    * Gets the plugin definition's id.
    *
    * @return string
+   *   The plugin id of this definition.
    */
-  public function getPluginId() : string ;
+  public function getPluginId() : string;
 
   /**
    * Gets the class name of the plugin.
@@ -39,8 +42,9 @@ interface PluginDefinitionInterface {
    * The returned class must implement \EclipseGc\Plugin\PluginInterface.
    *
    * @return string
+   *   The class name for the plugin represented by this definition.
    */
-  public function getClass() : string ;
+  public function getClass() : string;
 
   /**
    * Gets the class name of the factory.
@@ -48,7 +52,8 @@ interface PluginDefinitionInterface {
    * The returned class must implement \EclipseGc\Plugin\Factory\FactoryInterface.
    *
    * @return string
+   *   The class of the factory to instantiate the plugin. Can be empty string.
    */
-  public function getFactory() : string ;
+  public function getFactory() : string;
 
 }
