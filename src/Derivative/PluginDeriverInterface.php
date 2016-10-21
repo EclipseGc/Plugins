64e1754bd334fefcfceeb213ea53f887c001a800
Kris <kris@meridian-ds.com>
2016-10-20 21:22:24 -0500

    updating PluginDeriverInterface to conform to new coding standards.

--- src/Derivative/PluginDeriverInterface.php
+++ src/Derivative/PluginDeriverInterface.php
@@ -1,12 +1,13 @@
 <?php
 
-/**
- * @file
- * Contains \EclipseGc\Plugin\Derivative\PluginDeriverInterface.
- */
-
 namespace EclipseGc\Plugin\Derivative;
 
+/**
+ * Derives new plugin definitions from a single definition by some criteria.
+ *
+ * Derivers take a single plugin definition and generate new plugin definitions
+ * generally by looping over some other criteria.
+ */
 interface PluginDeriverInterface {
 
   /**
@@ -16,6 +17,7 @@ interface PluginDeriverInterface {
    *   The definition from which to derive new definitions.
    *
    * @return \EclipseGc\Plugin\PluginDefinitionInterface[]
+   *   An array of plugin definitions.
    */
   public function getDerivativeDefinitions(PluginDefinitionDerivativeInterface $definition);
 
