ba6fd08d0edcf19788344b36b934fc4613ef80fc
Kris <kris@meridian-ds.com>
2016-10-22 09:59:35 -0500

    updating to the new documentation standard.

--- src/Factory/FactoryResolverInterface.php
+++ src/Factory/FactoryResolverInterface.php
@@ -1,20 +1,27 @@
 <?php
 
-/**
- * @file
- * Contains \EclipseGc\Plugin\Factory\FactoryResolverInterface.
- */
-
 namespace EclipseGc\Plugin\Factory;
 
+/**
+ * An interface that abstracts the instantiation of plugins.
+ *
+ * Plugin often have dependencies external to the plugin system and might
+ * require any number of different tactics for instantiation like using a
+ * dependency injection container or some sort of static factory. In order to
+ * not introduce the logic in some plugin factory adapter, this interface
+ * provides a simple mechanism by which to satisfy this requirement as a base
+ * assumption of the plugin instantiation mechanism.
+ */
 interface FactoryResolverInterface {
 
   /**
    * Resolves a factory class string into a FactoryInterface object.
    *
    * @param string $factoryClass
+   *   A string representing the factory or a class name of the factory.
    *
    * @return FactoryInterface
+   *   The instantiated factory class.
    */
-  public function getFactoryInstance(string $factoryClass) : FactoryInterface ;
+  public function getFactoryInstance(string $factoryClass) : FactoryInterface;
 }
