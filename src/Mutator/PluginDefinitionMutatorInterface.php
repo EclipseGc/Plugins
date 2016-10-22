a30918705f8a19ba8e2c783214ff3951aa6838e4
Kris <kris@meridian-ds.com>
2016-10-22 11:27:09 -0500

    updating to the new documentation standard.

--- src/Mutator/PluginDefinitionMutatorInterface.php
+++ src/Mutator/PluginDefinitionMutatorInterface.php
@@ -1,22 +1,29 @@
 <?php
 
-/**
- * @file
- * Contains \EclipseGc\Plugin\Mutator\PluginDefinitionMutatorInterface.
- */
-
 namespace EclipseGc\Plugin\Mutator;
 
 use EclipseGc\Plugin\PluginDefinitionInterface;
 
+/**
+ * An interface for mutating plugin definitions as necessary during discovery.
+ *
+ * Plugin definitions may be mutated in any number of ways before being added
+ * to a set of definitions. This means a set of plugin definitions can be
+ * altered to include new definitions, exclude existing definition, expand one
+ * definition into many or any other sort of changes to the group of
+ * definitions which would affect all subsystems using the plugin type.
+ */
 interface PluginDefinitionMutatorInterface {
 
   /**
    * Mutates plugin definitions by altering or expanding them.
    *
-   * @param \EclipseGc\Plugin\PluginDefinitionInterface[] $definitions
+   * @param \EclipseGc\Plugin\PluginDefinitionInterface ...$definitions
+   *   The plugin definitions to mutate.
    *
    * @return \EclipseGc\Plugin\PluginDefinitionInterface[]
+   *   The mutated plugin definitions.
    */
   public function mutate(PluginDefinitionInterface ...$definitions) : array ;
+
 }
