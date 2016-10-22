cde23a2a64c53911a9a84c1d51c529c1bb9d9ce2
Kris <kris@meridian-ds.com>
2016-10-22 11:09:29 -0500

    updating to the new documentation standard.

--- src/Mutator/PluginDefinitionDeriverMutator.php
+++ src/Mutator/PluginDefinitionDeriverMutator.php
@@ -1,19 +1,25 @@
 <?php
 
-/**
- * @file
- * Contains \EclipseGc\Plugin\Filter\PluginDefinitionDeriverFilter.
- */
-
 namespace EclipseGc\Plugin\Mutator;
 
 use EclipseGc\Plugin\Derivative\PluginDefinitionDerivativeInterface;
 use EclipseGc\Plugin\Derivative\PluginDeriverResolverInterface;
 use EclipseGc\Plugin\PluginDefinitionInterface;
 
+/**
+ * A mutator which derives new definitions from definitions with a deriver.
+ *
+ * Plugin definitions may be mutated in any number of ways before being added
+ * to a set of definitions. The deriver mutator checks plugin definitions for
+ * implementation of the PluginDefinitionDerivativeInterface and extracts new
+ * plugin definitions via the referenced deriver class. This allows a single
+ * plugin class to be reference by multiple calculated plugin definitions.
+ */
 class PluginDefinitionDeriverMutator implements PluginDefinitionMutatorInterface {
 
   /**
+   * The instantiated plugin deriver resolver.
+   *
    * @var \EclipseGc\Plugin\Derivative\PluginDeriverResolverInterface
    */
   protected $deriverResolver;
