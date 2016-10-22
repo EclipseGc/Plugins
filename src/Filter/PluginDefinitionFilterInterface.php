2e6d88bf20b76399fa926af0d2703c17ca10d63b
Kris <kris@meridian-ds.com>
2016-10-22 10:27:30 -0500

    updating to the new documentation standard.

--- src/Filter/PluginDefinitionFilterInterface.php
+++ src/Filter/PluginDefinitionFilterInterface.php
@@ -1,22 +1,22 @@
 <?php
 
-/**
- * @file
- * Contains \EclipseGc\Plugin\Discovery\PluginDefinitionFilterInterface.
- */
-
 namespace EclipseGc\Plugin\Filter;
 
 use EclipseGc\Plugin\PluginDefinitionInterface;
 
+/**
+ * An interface for determining what plugins belong in a filtered set.
+ */
 interface PluginDefinitionFilterInterface {
 
   /**
    * Filters available plugins in the plugin discovery object.
    *
    * @param \EclipseGc\Plugin\PluginDefinitionInterface $definition
+   *   The current plugin definition being evaluated.
    *
    * @return bool
+   *   Boolean return to indicate inclusion or exclusion of the definition.
    */
   public function filter(PluginDefinitionInterface $definition) : bool;
 
